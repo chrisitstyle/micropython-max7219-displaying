4683b75743c17be9c5836db2ad76f7a2fe8ab1f6
Krzysztof Podjacki <infernos1244@o2.pl>
2023-12-09 20:13:18 +0100

    polish comments circular_buffer

--- circular_buffer.py
+++ circular_buffer.py
@@ -1,54 +1,45 @@
 class CircularBuffer(object):
 
     def __init__(self, max_size=10):
-        """Initialize the CircularBuffer with a max_size if set, otherwise
-        max_size will elementsdefault to 10"""
+        """inicjalizacja parametrow bufora"""
         self.buffer = [None] * max_size
         self.head = 0
         self.tail = 0
         self.max_size = max_size
 
     def __str__(self):
-        """Return a formatted string representation of this CircularBuffer."""
+        """zwracanie sformatowanego stringa"""
         items = ['{!r}'.format(item) for item in self.buffer]
         return '[' + ', '.join(items) + ']'
 
     def size(self):
-        """Return the size of the CircularBuffer
-        Runtime: O(1) Space: O(1)"""
+        """Zwracanie rozmiaru bufora"""
         if self.tail >= self.head:
             return self.tail - self.head
         return self.max_size - self.head - self.tail
 
     def is_empty(self):
-        """Return True if the head of the CircularBuffer is equal to the tail,
-        otherwise return False
-        Runtime: O(1) Space: O(1)"""
+        """Zwracanie true, jeśli bufor jest pusty"""
         return self.tail == self.head
 
     def is_full(self):
-        """Return True if the tail of the CircularBuffer is one before the head,
-        otherwise return False
-        Runtime: O(1) Space: O(1)"""
+        """zwracanie true, jeśli ogon jest przed glowa """
         return self.tail == (self.head-1) % self.max_size
 
     def enqueue(self, item):
-        """Insert an item at the back of the CircularBuffer
-        Runtime: O(1) Space: O(1)"""
+        """Wstawianie elementu na koniec bufora"""
         if self.is_full():
             raise OverflowError(
-                "CircularBuffer is full, unable to enqueue item")
+                "Bufor jest pełny, nie mozna zapisac elementu")
         self.buffer[self.tail] = item
         self.tail = (self.tail + 1) % self.max_size
 
     def front(self):
-        """Return the item at the front of the CircularBuffer
-        Runtime: O(1) Space: O(1)"""
+        """Zwraca element z przodu bufora"""
         return self.buffer[self.head]
 
     def dequeue(self):
-        """Return the item at the front of the Circular Buffer and remove it
-        Runtime: O(1) Space: O(1)"""
+        """Zwraca element z przodu bufora i usuwa"""
         if self.is_empty():
             raise IndexError("CircularBuffer is empty, unable to dequeue")
         item = self.buffer[self.head]
